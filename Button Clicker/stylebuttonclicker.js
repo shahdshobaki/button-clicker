8270ee2d776a81ead197ffc0eff8d6f375ab0ef8
Shahd Shobaki <shahdshobaki79@gmail.com>
2024-05-28 11:28:18 +0300

    Add files via upload

---
+++ Button Clicker/stylebuttonclicker.js
@@ -0,0 +1,12 @@
+
+function changeMe(element){
+element.innerText= "logout";
+}
+
+function removeMe(element){
+    element.remove();
+}
+function likeMe(element){
+    alert("Ninja was liked");
+}
+
